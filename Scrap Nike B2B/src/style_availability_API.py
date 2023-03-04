e9e07c014f7579123a2981f3652bda28c308bedb
Mohammad Saud <105937990+SaudZafeer@users.noreply.github.com>
2023-03-04 14:52:55 -0600

    Update style_availability_API.py

--- Scrap Nike B2B/src/style_availability_API.py
+++ Scrap Nike B2B/src/style_availability_API.py
@@ -9,7 +9,19 @@ class ColorId:
         querystring = {"avmig":"HO19","childSo":"1000","closeout":"exclude","contracts":"0042270969-1000,0043095513-1000,0042234363-1000,0042258166-1000,0041977603-1000","currency":"USD","extContracts":"none","language":"EN","limit":"400","minAv":"1","page":"1","priceList":"USA","q":"Jordan","region":"USA","sizeType":"USA","so":"1000","soldTo":"0000017835","type":"FTWR"}
 
         payload = ""
-        headers = { #add your Headers here}
+        #Fill your headers in order to run this code
+        headers = {
+    "User-Agent": "",
+    "Accept": "",
+    "Accept-Language": "",
+    "Accept-Encoding": "",
+    "Connection": "",
+    "Referer": "https://atonce.nike.net/",
+    "Cookie": '',
+    "Sec-Fetch-Dest": "",
+    "Sec-Fetch-Mode": "",
+    "Sec-Fetch-Site": ""
+}
         response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
         # print(response.text)
         for items in response.json()['products']:
